6d38e729170104b81ea943a43bac46ee746ecc2e
MadMcCrow <noe.perard+git@gmail.com>
2023-02-28 21:59:43 +0100

    bump version

--- version.nix
+++ version.nix
@@ -2,7 +2,7 @@
 # get godot version
 { system }: {
   # this builds godot 4. we should instead read it from the input file
-  version = "4.0-beta";
+  version = "4.0-rc";
   # todo : test darwin support
   platform = if (system == "x86_64-linux") then "linuxbsd" else "darwin";
 }
