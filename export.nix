d16be1aa51c0c264d16db261803b8ab7150a903a
MadMcCrow <noe.perard+git@gmail.com>
2023-10-09 18:47:56 +0200

    WIP export games

--- export.nix
+++ export.nix
@@ -3,13 +3,17 @@
 { pkgs, ... }:
 { godot, src, name, nativeBuildInputs ? [], ... } @args :
 with pkgs;
+with builtins;
 let
 
   templateName = pkgs.system;
   linux_ext = ".bin";
 
+  # remove args not used in mkDerivation or merged manually
+  buildArgs = removeAttrs args ["godot" "nativeBuildInputs"];
+
   # result derivation
-in stdenv.mkDerivation (args // {
+in stdenv.mkDerivation ({
   inherit name src;
   nativeBuildInputs = [ godot breakpointHook ] ++ nativeBuildInputs;
   buildPhase = ''${godot}/bin/godot --export ${templateName} ${name}${linux_ext}'';
@@ -18,4 +22,4 @@ in stdenv.mkDerivation (args // {
     ls -la
     breakpointHook
   '';
- })
+ } // buildArgs)
